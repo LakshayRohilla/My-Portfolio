69d0aada6896a8846fa1be0eaf9b8beeec12913e
LakshayRohilla10 <lakshay.rohilla@sms-group.com>
2023-08-27 17:39:18 +0530

    Navbar is added

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Navbar.css"
+
+export default function Navbar() {
+    return(
+        <div className="n-wrapper">
+            <div className="n-left">
+                <div className="n-name">
+                    Lakshay
+                </div>
+                <span>toggle</span>
+            </div>
+            <div className="n-right">
+                <div className="n-list">
+                    <ul style={{listStyleType:'none'}}>
+                        <li>Home</li>
+                        <li>Experience</li>
+                        <li>Education</li>
+                        <li>Projects</li>
+                        <li>Certificates</li>
+                    </ul>
+                </div>
+                <button className="button n-button">
+                    Contact Me
+                </button>
+            </div>
+        </div>
+    )
+}
+
+
