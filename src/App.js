a339ad58f7800cd8f42b6e166610257e8b6840d5
LakshayRohilla10 <lakshay.rohilla@sms-group.com>
2023-08-31 10:31:14 +0530

    Added education section completely

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./Components/Navbar/Navbar";
 import './App.css';
 import Intro from "./Components/Intro/Intro";
 import Skills from "./Components/Skills/Skills";
+import Education from "./Components/Education/Education";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar/>
       <Intro/>
       <Skills/>
+      <Education/>
     </div>
   );
 }
